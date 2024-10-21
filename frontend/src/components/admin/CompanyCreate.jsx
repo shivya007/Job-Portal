afc03149ef5981f64c6dd95f053cb363e3c3b4d9
Shiv Arora <passionshiv007@gmail.com>
2024-10-21 08:40:40 +0530

    Full Stack Job Portal Application

--- frontend/src/components/admin/CompanyCreate.jsx
+++ frontend/src/components/admin/CompanyCreate.jsx
@@ -29,7 +29,7 @@ const CompanyCreate = () => {
                 navigate(`/admin/companies/${companyId}`);
             }
         } catch (error) {
-            console.log(error);
+            console.log("seeeeeeeeeeeee error",error);
         }
     }
   return (
